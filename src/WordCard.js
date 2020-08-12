49682665d76f4457d18f0bc6c0040e50ad0b5f92
TUNYONG THIPWONGMEK (ตันหยง ทิพย์วงศ์เมฆ) <6110110156@email.psu.ac.th>
2020-08-13 02:49:39 +0700

    part5

--- src/WordCard.js
+++ src/WordCard.js
@@ -2,10 +2,18 @@ import React from 'react';
 import CharacterCard from './CharacterCard';
 
 export default function Wordcard(prons){
+   
+    const activationHandler = c => 
+    { 
+        console.log(`${c} has been activated.`) 
+    }
+   
     return(
     <div>
        {
-        Array.from(prons.value).map((c,i)=> <CharacterCard value={c} key={i}/>)
+        Array.from(prons.value).map((c,i)=>
+         <CharacterCard value={c} key={i} activationHandler={activationHandler}/>
+         )
       }
      
 
