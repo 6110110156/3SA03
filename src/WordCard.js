aafa1d346dbd64846637b0b9c62c699262682268
TUNYONG THIPWONGMEK (ตันหยง ทิพย์วงศ์เมฆ) <6110110156@email.psu.ac.th>
2020-08-15 19:58:36 +0700

    part 6

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import _, { attempt } from 'lodash';
+import _  from 'lodash';
 import CharacterCard from './CharacterCard';
 
 const prepareStateFromWord = (given_word) => {
