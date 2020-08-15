f105ef0bd9578342e31a16861489b42cd2744b67
TUNYONG THIPWONGMEK (ตันหยง ทิพย์วงศ์เมฆ) <6110110156@email.psu.ac.th>
2020-08-15 21:18:54 +0700

    Oderandcss

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Wordcard from './WordCard';
 const number ="1234567890"; //เปลี่ยนเป็นตัวเลข
 function App() {
   return (
-    <div className='Appst'>
+    //เพิ่มclassName Appst
+    //เพิ่มคำสั่งและชื่อเกมส์
+    <div className='Appst'> 
+      <h1>GAME EVEN NUMBER</h1>
+      <h2>please select an even number(5 number)</h2>
      <Wordcard value={number}/>
     </div>
   );
